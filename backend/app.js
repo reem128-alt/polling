b64f45b9ec93db14b32f6462f3c0233eb094fe0d
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-03-16 02:16:44 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3001","http://localhost:3000", 
-      "https://blog-beta-seven-98.vercel.app/",
+      "https://polling-tau.vercel.app/",
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
